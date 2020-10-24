57f771372b748b8db6d51957e8bc59255c45df74
William Eustace <weustace@wje.io>
2020-10-24 15:41:28 +0100

    Commented better

--- trie_generic.cpp
+++ trie_generic.cpp
@@ -93,29 +93,35 @@ class PrefixTree {
     std::vector<PossibleState> findPossibleChildren(const std::vector<T> partial,const P Pc, const P Pi, const P Pm, const P prob_thresh){   
         std::vector<PossibleState> current_states;
         current_states.push_back(this->root.get());
-        for(T c : partial){
+        for(T c : partial){//For each char in input
             std::cout<<current_states.size()<<std::endl;
             std::unordered_map<Node<T,P>*,unsigned int> counts;
             const int current_state_size = current_states.size();
-            for(int i=0;i<current_state_size;i++){
+            for(int i=0;i<current_state_size;i++){//for each of the states currently under consideration...
                 const PossibleState state = current_states[i];
-                if(counts.count(state.current)){
+
+                if(counts.count(state.current)){ //next few lines are in preparation for proper state combination
                     counts[state.current]=counts[state.current]+1;
                 }
                 else{
                     counts.insert({state.current,1});
                 }
-                if(state.running_prob < prob_thresh)
+
+
+                if(state.running_prob < prob_thresh)//if the probability of the state is below some threshold, forget it...we should also pop it 
                     continue;
-                
+
+
+                //beginning of update logic (currently flawed)               
                 if(state.current->children.size()==0){//if no child state, must be an insertion error
                     current_states[i].running_prob += std::log(Pi);
                 }
                 else if(state.current->children.count(c) == 0){//if none of the children match
+                auto n_children = state.current->children.size();
                     for(auto &child_pair : state.current->children){
                         auto new_state = state;
                         new_state.current = child_pair.second.get();
-                        new_state.running_prob += std::log(Pm);//we know that the current char can't be correct, so it must be incorrect or a false insertion
+                        new_state.running_prob += std::log(Pm/n_children);//we know that the current char can't be correct, so it must be incorrect or a false insertion
                         new_state.trace_of_current_state.push_back(child_pair.first);
                         if(new_state.running_prob>prob_thresh)
                             current_states.push_back(new_state);
@@ -123,7 +129,7 @@ class PrefixTree {
                     current_states[i].running_prob += std::log(Pi);
                 }
                 else {//case with matching child
-                    auto n_other_children = state.current->children.size() -1;
+                    auto n_other_children = state.current->children.size() + (-1 ? state.current->children.size()>1 : 0);
                     for(auto &child_pair : state.current->children){
                         if(child_pair.first != c){
                             PossibleState new_state = state;
@@ -135,8 +141,6 @@ class PrefixTree {
                         }
                     }
                     auto new_state = state;
-                    // std::cout<<new_state.current<<"\n";
-                    // std::cout<<state.current<<"\n"<<std::endl;
                     new_state.current = (state.current->children[c]).get();
                     new_state.running_prob += std::log(Pc);//Correct char
                     new_state.trace_of_current_state.push_back(c);
@@ -145,7 +149,8 @@ class PrefixTree {
                     current_states[i].running_prob += std::log(Pi); //this char shouldn't have been typed at all
                 }
             }
-            std::cout<<counts.size()<<"========="<<std::endl;
+
+            std::cout<<counts.size()<<"========="<<std::endl;//counts.size() gives the *actual* number of states...<<current_states.size()
         }
         return current_states;
     }
